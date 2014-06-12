d72d2940cb6e7aab98195ce948c4b6619716e9de
Slice <matt.gray@retrofuzz.com>
2014-06-12 14:03:31 +0100

    Enabled debugging in Laravel, made the gruntfile work + livereload

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,15 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var svgSprites = require('gulp-svg-sprites');
 var livereload = require('gulp-livereload');
+var LIVERELOAD_PORT = 35729;
 
 gulp.task('default', ['lint', 'sass', 'js']);
 
 gulp.task('sass', function() {
 	return gulp.src('assets/scss/*.scss')
 		.pipe(sass())
-		.pipe(gulp.dest('public_html/assets/css'));
+		.pipe(gulp.dest('public_html/assets/css'))
+		.pipe(livereload({ auto: false }));
 });
 
 gulp.task('js', function() {
@@ -20,7 +22,8 @@ gulp.task('js', function() {
 		'assets/js/objects/*.js'
 	])
 	.pipe(concat('script.js'))
-	.pipe(gulp.dest('./public_html/assets/js'));
+	.pipe(gulp.dest('./public_html/assets/js'))
+	.pipe(livereload({ auto: false }));
 });
 
 gulp.task('lint', function() {
@@ -32,6 +35,7 @@ gulp.task('production', function() {
 });
 
 gulp.task('watch', function() {
+	livereload.listen(LIVERELOAD_PORT);
 	gulp.watch('assets/js/*.js', ['js']);
 	gulp.watch('assets/scss/*.scss', ['sass']);
 });
