7ea63e1d34a937a5dec88b48f5b74e0c97ab158e
Simon Archer <simon.archer@retrofuzz.com>
2014-08-05 17:53:29 +0100

    Added basic client-server comms.

--- app/views/pages/parties/show.blade.php
+++ app/views/pages/parties/show.blade.php
@@ -1,5 +1,5 @@
 @extends(Input::get('pagefragment', false) === false?'templates.master':'templates.ajax')
 
-<div class="party-page" data-slug="{{ $party->slug }}">
+<div class="party-page" data-init="party-page" data-slug="{{ $party->slug }}">
 	<h1>{{ $party->name }}</h1>
 </div>
--- assets/js/objects/party-page.js
+++ assets/js/objects/party-page.js
@@ -5,11 +5,40 @@ initScripts['party-page'] = function(element) {
 		type: "party-page",
 		element: element,
 		slug: $(element).attr('data-slug'),
-		destroy: destroy
+		destroy: destroy,
+		isDj: false,
+		pollTimeout: null,
+		playingTrack: null
 	};
 
 	// Connect to the party.
-	
+	var req = getRequestObject();
+	req.partySlug = object.slug
+	socket.emit(EVENT_JOIN_PARTY, req);
+
+	// Listen for events.
+	socket.on(EVENT_DJ_ASSIGN, function(){
+		console.log('Assigned as DJ', req);
+		setDj();
+    });
+
+	socket.on(EVENT_LISTENER_SYNC, function(req){
+		if (req.partySlug == object.slug && !object.isDj){
+			console.log('Listener sync', req);
+		}
+    });
+
+	function setDj(){
+		object.isDj = true;
+		object.pollInterval = setInterval(pollServer, POLL_INTERVAL);
+	}
+
+	function pollServer(){
+		var req = getRequestObject();
+		req.trackId = object.playingTrack == null ? -1 : playingTrack.spotifyId
+
+		socket.emit(EVENT_DJ_POLL, req);
+	}
 
 	function destroy(){
 	}
